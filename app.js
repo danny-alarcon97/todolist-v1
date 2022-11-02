4e861a30ac0ae29fbe56b313734db37d3f21d481
daniel <danny.alarcon1997@gmail.com>
2022-11-02 14:30:29 -0400

    passing data from webpage to server

--- app.js
+++ app.js
@@ -5,63 +5,38 @@ const ejs = require("ejs");
 const app = express();
 const port = 3000;
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-    default:
-      console.log("Error: current day is equal to:" + currentDay);
-      break;
-  }
-
-  // if (currentDay === 0) {
-  //   day = "Sunday";
-  // } else if (currentDay === 1) {
-  //   day = "Monday";
-  // } else if (currentDay === 2) {
-  //   day = "Tuesday";
-  // } else if (currentDay === 3) {
-  //   day = "Wednesday";
-  // } else if (currentDay === 4) {
-  //   day = "Thursday";
-  // } else if (currentDay === 5) {
-  //   day = "Friday";
-  // } else {
-  //   day = "Saturday";
-  // }
+  var day = today.toLocaleDateString("en-US", options);
 
   // creating a response by rendering a file called list,
   // which has to exist inside a views folder and has to have the extension .ejs.
   // And then into that list file we're passing in a single variable that has the name of kindOfDay,
   // and the value that we're giving it is the value of our variable, day.
 
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+// post request for value of newItem form
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 app.listen(port, function () {
