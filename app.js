f18c0359467027286081cc23049dfb65593ef5a5
daniel <danny.alarcon1997@gmail.com>
2022-11-02 21:05:19 -0400

    node module exports, passing function from date.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 const port = 3000;
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = ["Log In", "Respond To Emails", "Review Project"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = ["Log In", "Respond To Emails", "Review Project"];
 
 app.set("view engine", "ejs");
 
@@ -14,27 +15,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
   // creating a response by rendering a file called list,
   // which has to exist inside a views folder and has to have the extension .ejs.
   // And then into that list file we're passing in a single variable that has the name of kindOfDay,
   // and the value that we're giving it is the value of our variable, day.
+
+  let day = date.getDate();
 
   res.render("list", { listTitle: day, newListItems: items });
 });
 
 // post request for value of newItem form
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list === "Work") {
     workItems.push(item);
@@ -50,7 +43,7 @@ app.get("/work", function (req, res) {
 });
 
 app.post("/work", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 });
